0d8401fd8e2af2ad766242e81792f649161a420d
Charlotte Sarfati <csarfati@Charlotte-Sarfati-MacBook-Pro.local>
2019-04-09 09:10:34 -0700

    script specs outlined

---
+++ maintenance-window.py
@@ -0,0 +1,7 @@
+# SCRIPT SPECS
+    # use pdpyras so its python 3 compatible (requirements.txt)
+    # hardcode just ONE service ID, point out w/ #
+    # start_time = current, end_time = +2hr
+    # just ONE maintenance window, not recurring
+    # hardcode API key, point out w/ #
+    # Description = "PagerDuty Maintenance Window"
