227377c39505b4ee56e616645b92bb1bcb783fa3
Charlotte Sarfati <csarfati@Charlotte-Sarfati-MacBook-Pro.local>
2019-04-09 09:33:05 -0700

    completed pseudocode

--- maintenance-window.py
+++ maintenance-window.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python
+
+# Python script to create a 2-hour maintenance window on one PD service
+
 # SCRIPT SPECS
     # use pdpyras so its python 3 compatible (requirements.txt)
     # hardcode just ONE service ID, point out w/ #
@@ -5,3 +9,41 @@
     # just ONE maintenance window, not recurring
     # hardcode API key, point out w/ #
     # Description = "PagerDuty Maintenance Window"
+
+# EXAMPLE PAYLOAD
+# {
+#   "maintenance_window": {
+#     "type": "maintenance_window",
+#     "start_time": ""  # CURRENT
+#     "end_time": " ""  # CURRENT +2hr
+#     "description": "PagerDuty Maintenance Window",
+#     "services": [
+#       {
+#         "id": "pd-service-id", ## INPUT SERVICE ID
+#         "type": "service_reference"
+#       }
+#     ]
+#   }
+# }
+
+
+import pdpyras
+import json
+import dateutil.parser as dateparser
+from datetime import datetime, timedelta
+
+
+api_token = 'input-api-token'  # input API key
+session = pdpyras.APISession(api_token)
+
+
+def create_maintenance_window():
+
+    # initialize hardcoded variables
+    # print "starting" message
+    # post request w/ example payload
+    # print response message / error handling
+    exit()
+
+if __name__ == '__main__':
+    create_maintenance_window()
