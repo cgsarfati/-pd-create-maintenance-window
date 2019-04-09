9c9fb5213b5192b2673c70aa85ee32860d34733f
Charlotte Sarfati <csarfati@Charlotte-Sarfati-MacBook-Pro.local>
2019-04-09 10:19:13 -0700

    completed barebones script, tested successfully. added HTTP error handling.

--- maintenance-window.py
+++ maintenance-window.py
@@ -1,49 +1,45 @@
 #!/usr/bin/env python
 
 # Python script to create a 2-hour maintenance window on one PD service
-
-# SCRIPT SPECS
-    # use pdpyras so its python 3 compatible (requirements.txt)
-    # hardcode just ONE service ID, point out w/ #
-    # start_time = current, end_time = +2hr
-    # just ONE maintenance window, not recurring
-    # hardcode API key, point out w/ #
-    # Description = "PagerDuty Maintenance Window"
-
-# EXAMPLE PAYLOAD
-# {
-#   "maintenance_window": {
-#     "type": "maintenance_window",
-#     "start_time": ""  # CURRENT
-#     "end_time": " ""  # CURRENT +2hr
-#     "description": "PagerDuty Maintenance Window",
-#     "services": [
-#       {
-#         "id": "pd-service-id", ## INPUT SERVICE ID
-#         "type": "service_reference"
-#       }
-#     ]
-#   }
-# }
+# CLI usage: python maintenance-window.py
 
 
 import pdpyras
-import json
-import dateutil.parser as dateparser
 from datetime import datetime, timedelta
 
 
-api_token = 'input-api-token'  # input API key
+api_token = 'api-key-here'  # API KEY HERE
 session = pdpyras.APISession(api_token)
 
 
 def create_maintenance_window():
 
-    # initialize hardcoded variables
-    # print "starting" message
-    # post request w/ example payload
-    # print response message / error handling
-    exit()
+    # set up time
+    start_time = datetime.now()
+    end_time = start_time + timedelta(minutes=120)
+
+    print("Creating maintenance window...")
+
+    # create window
+    try:
+        api_request = session.rpost('maintenance_windows', json={
+            'type': 'maintenance_window',
+            'start_time': start_time.isoformat(),
+            'end_time': end_time.isoformat(),
+            'description': "PagerDuty Maintenance Window",
+            'services': [
+                {'id': 'service-id-here',  # SERVICE ID HERE
+                    'type': 'service_reference'}]
+            })
+        print("Maintenance window successfully created")
+
+    # error handling
+    except pdpyras.PDClientError as e:
+        msg = "API Error: "
+        if e.response is not None:
+            msg += "HTTP %d" % (e.response.status_code)
+            print(msg)
+
 
 if __name__ == '__main__':
     create_maintenance_window()
